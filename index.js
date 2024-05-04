9a49e5b1c85b8d5497e0d07f2c9a8817e222a5f8
adhimrahman <adhimrahaman@gmail.com>
2024-05-04 08:53:34 +0800

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+app.set('view engine', 'ejs');
 // app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/", (req, res) => {
     res.render('login')
